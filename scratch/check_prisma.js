07cec72eba20a51a8de7260398d2ed8dc7af51c4
Omobolaji Durojaiye <durojaiyeomobolaji93@gmail.com>
2026-04-21 17:55:57 +0100

    chore: update gitignore for cleaner repository structure

--- scratch/check_prisma.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  console.log('Models available in prisma client:');
-  console.log(Object.keys(prisma).filter(k => !k.startsWith('_')));
-}
-
-main().catch(e => console.error(e)).finally(() => prisma.$disconnect());
