5851bb1d639b3415e05a59b8918e88a28efffb2a
Omobolaji Durojaiye <durojaiyeomobolaji93@gmail.com>
2026-05-01 22:18:53 +0100

    sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://www.techbe.online';
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/'],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
